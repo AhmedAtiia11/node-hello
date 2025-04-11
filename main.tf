80d44f7cd18b3f17f3ea25ee0421481cc25936ca
Ahmedatiia11 <ahmed@example.com>
2025-04-11 23:04:10 +0200

    add terraform main file

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreu/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "app_image" {
+  name = "ahmedatiia11/node-hello:latest"
+}
+
+resource "docker_container" "app_container" {
+  name  = "app"
+  image = docker_image.app_image.image_id
+  ports {
+    internal = 3000
+    external = 3000
+  }
+}
