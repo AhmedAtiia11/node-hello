db32ba3a7e877bdc80339c35a2bfa47e1c998251
Ahmedatiia11 <ahmed@example.com>
2025-04-12 22:37:48 +0200

    modify main.tf file

--- main.tf
+++ main.tf
@@ -1,53 +1,23 @@
 terraform {
   required_providers {
     docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.0.2"
+      source  = "kreuzwerker/docker"  # Correct provider source
+      version = "3.0.2"             # Check for the latest version if needed
     }
   }
 }
 
-provider "docker" {
-  registry_auth {
-    address  = "registry-1.docker.io"  
-    username = var.dockerhub_username
-    password = var.dockerhub_password
-  }
-}
-
-variable "dockerhub_username" {
-  type = string
-}
-
-variable "dockerhub_password" {
-  type      = string
-  sensitive = true
-}
+provider "docker" {}
 
 resource "docker_image" "app_image" {
-  name = "registry-1.docker.io/ahmedatiia11/node-hello:latest"
-  build {
-    context    = "."
-    dockerfile = "Dockerfile"
-    tag        = ["registry-1.docker.io/ahmedatiia11/node-hello:latest"]
-  }
-}
-
-resource "docker_registry_image" "app_image" {
-  name          = docker_image.app_image.name
-  keep_remotely = true  # Keep image in registry even after destroy
+  name         = "ahmedatiia11/node-hello:latest"
 }
 
 resource "docker_container" "app_container" {
-  name         = "app"
-  image        = docker_image.app_image.image_id
-
-  
+  name  = "app"
+  image = docker_image.app_image.image_id
   ports {
     internal = 3000
     external = 3000
   }
-
-  restart = "unless-stopped"  # Add restart policy to handle crashes
 }
-
