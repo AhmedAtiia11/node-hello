0f23c939306f4f1298c8797340829d57991bf174
Ahmedatiia11 <ahmed@example.com>
2025-04-12 16:43:00 +0200

    test change in docker image

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node! on 12 april 2025 \n'
+  const msg = 'Hello Node! test change in docker image \n'
   res.end(msg);
 });
 
