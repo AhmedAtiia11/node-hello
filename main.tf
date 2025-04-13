b617afb7600eb7cff21e36af4f4f9f70e24a01e7
Ahmedatiia11 <ahmed@example.com>
2025-04-13 16:24:51 +0200

    fix docker push issue

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ terraform {
     }
   }
 
-  # # Terraform Cloud configuration
   cloud {
     organization = "nawy"
     workspaces {
@@ -15,24 +14,53 @@ terraform {
   }
 }
 
-provider "docker" {}
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
 
-resource "docker_image" "app_image" {
-  name         = "ahmedatiia11/node-hello:latest"
-  keep_locally = false
+
+  registry_auth {
+    address  = "registry-1.docker.io"  
+    username = var.dockerhub_username
+    password = var.dockerhub_password
+  }
 }
 
+# Build local image
+resource "docker_image" "app_image" {
+  name         = "ahmedatiia11/node-hello:latest"  
+  keep_locally = false 
+}
+
+# Push to Docker Hub
+resource "docker_registry_image" "app_image" {
+  name = docker_image.app_image.name
+}
+
+
+
+# Container deployment
 resource "docker_container" "app_container" {
   name  = "app"
   image = docker_image.app_image.image_id
   env = [
     "SERVICE=elastic",
     "NEW_RELIC_LICENSE_KEY=114beed378df1617048e4c8abdfdf59cFFFFNRAL",
-    "NEW_RELIC_APP_NAME=node-hello-app"  # Fixed space before variable name
+    "NEW_RELIC_APP_NAME=node-hello-app"
   ]
 
   ports {
     internal = 3000
     external = 3000
   }
+}
+
+# Variable definitions
+variable "dockerhub_username" {
+  type      = string
+  sensitive = true
+}
+
+variable "dockerhub_password" {
+  type      = string
+  sensitive = true
 }
