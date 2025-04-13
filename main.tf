4c99ff92bb6f5698988a5669af9c7a31ccd0979d
Ahmedatiia11 <ahmed@example.com>
2025-04-13 14:34:27 +0200

    add remote backend for state file

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
 terraform {
   required_providers {
     docker = {
-      source  = "kreuzwerker/docker"  # Correct provider source
-      version = "3.0.2"             # Check for the latest version if needed
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+
+  # Terraform Cloud configuration
+  cloud {
+    organization = "nawy"
+    workspaces {
+      name = "task"
     }
   }
 }
@@ -11,12 +19,17 @@ provider "docker" {}
 
 resource "docker_image" "app_image" {
   name         = "ahmedatiia11/node-hello:latest"
+  keep_locally = false
 }
 
 resource "docker_container" "app_container" {
   name  = "app"
   image = docker_image.app_image.image_id
-  env = ["SERVICE=elastic", "NEW_RELIC_LICENSE_KEY=114beed378df1617048e4c8abdfdf59cFFFFNRAL"," NEW_RELIC_APP_NAME=node-hello-app"]
+  env = [
+    "SERVICE=elastic",
+    "NEW_RELIC_LICENSE_KEY=114beed378df1617048e4c8abdfdf59cFFFFNRAL",
+    "NEW_RELIC_APP_NAME=node-hello-app" 
+  ]
 
   ports {
     internal = 3000
